f7b49a5823468da99929838a6384d93cda03109e
Jway <jway4347@gmail.com>
2026-04-28 08:25:04 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import {
 } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <Box>
-      <Container>
-        <VStack>
-          <Box>
-            <Heading>Yapper</Heading>
-            <Text>A simple Twitter clone built with Vite and Chakra UI.</Text>
-          </Box>
-        </VStack>
-      </Container>
-    </Box>
-  );
+return (
+  <Box bg="gray.900" minH="100vh" py={8}>
+    <Container maxW="650px">
+      <VStack gap={5} align="stretch">
+        <Box bg="gray.800" p={6} borderRadius="2xl" boxShadow="md">
+          <Heading size="lg" color="white">
+            Yapper
+          </Heading>
+          <Text color="gray.400" mt={2}>
+            A simple Twitter clone built with Vite and Chakra UI.
+          </Text>
+        </Box>
+      </VStack>
+    </Container>
+  </Box>
+);
 }
 
 export default App;
