2513ab19ab73f18e48c0d556e3fadd7ce447f156
Jway <jway4347@gmail.com>
2026-05-04 08:12:02 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { Provider } from "./components/ui/provider"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from "@/components/ui/provider"
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <Provider>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
